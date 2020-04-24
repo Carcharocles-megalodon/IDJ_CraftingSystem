df2b4bc19ee5f62ae816252b309aae861a0aa0e8
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-04-24 19:03:44 +0100

    still figuring it out

---
+++ Crafting_System/Crafting_System/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crafting_System
+{
+    class Item
+    {
+        public string Name { get; set; }
+
+        public uint Tier { get; set; }
+
+
+        public Item(string Name, uint Tier)
+        {
+            this.Name = Name;
+            this.Tier = Tier;
+        }
+    }
+}
---
+++ Crafting_System/Crafting_System/ItemMaterial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crafting_System
+{
+    class ItemMaterial : Item
+    {
+
+    }
+}
