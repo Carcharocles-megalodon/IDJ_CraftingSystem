054026780f81b29dc89dd38fb123773aae1285e9
Carcharocles-megalodon <55915879+Carcharocles-megalodon@users.noreply.github.com>
2020-05-05 19:23:29 +0100

    added Recipe and RecipeManager prototypes

--- Crafting_System/Crafting_System/Recipe.cs
+++ Crafting_System/Crafting_System/Recipe.cs
@@ -6,5 +6,13 @@ namespace Crafting_System
 {
     class Recipe
     {
+        Item [] requiredItems;
+        Item outputItem;
+        bool learned = false;
+        public Recipe(Item[]requiredItems, Item outputItem)
+        {
+            this.requiredItems = requiredItems;
+            this.outputItem = outputItem;
+        }
     }
 }
---
+++ Crafting_System/Crafting_System/RecipeManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crafting_System
+{
+    class RecipeManager
+    {
+        List<Recipe> recipeList;
+
+        public void Add(Recipe toAdd)
+        {
+            recipeList.Add(toAdd);
+        }
+    }
+}
